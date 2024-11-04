84bf391a27ce943f5c53beaee5986fb375308e04
elminhuseynov <elmin.huseynov0821@gmail.com>
2024-11-04 16:02:25 +0400

    lesson

---
+++ app.js
@@ -0,0 +1,10 @@
+let user = prompt("Soyadinizi ve adinizi daxil edin");
+console.log("Salam," + user.split(" ")[1] + "!" );
+
+let num = prompt("4 reqemli eded daxil edin");
+let num1 = (Number(num[0]));
+let num2 = (Number(num[1]));
+let num3 = (Number(num[2]));
+let num4 = (Number(num[3]));
+let result = num1 + num2 + num3 + num4;
+console.log(result);
